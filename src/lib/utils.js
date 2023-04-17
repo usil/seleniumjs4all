a8877721a21646e8bf9c0ba5c5cb75b24262259b
Diego Ramos <1625215555darn@gmail.com>
2023-04-17 16:14:47 -0500

    fix: added jsdoc

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,12 @@
-const { until, By } = require("selenium-webdriver");
+const { until, By, WebDriver, WebElement, WebElementCondition } = require("selenium-webdriver");
 
 const utils = {
+    /**
+     * 
+     * @param {WebDriver} driver WebDriver client, which provides control over a browser
+     * @param {String} buttonName Name of button element
+     * @returns Button element
+     */
     getAnyButton: async (driver, buttonName) => {
         const anyButton = await driver.wait(
           until.elementLocated(
@@ -12,6 +18,12 @@ const utils = {
         );
         return anyButton;
     },
+    /**
+     * 
+     * @param {WebDriver} driver WebDriver client, which provides control over a browser
+     * @param {String} buttonName Name of button element
+     * @returns Button element
+     */
     getAnyButtonAndClick: async (driver, buttonName) => {
         const anyButton = await driver.wait(
             until.elementLocated(
@@ -24,30 +36,76 @@ const utils = {
         await driver.executeScript("arguments[0].click();", anyButton);
         return anyButton;
     },
+    /**
+     * 
+     * @param {WebDriver} driver WebDriver client, which provides control over a browser
+     * @param {String} url website url to open
+     */
     openPage: async (driver, url) => {
         await driver.get(url);
     },
+    /**
+     * 
+     * @param {WebDriver} driver WebDriver client, which provides control over a browser
+     */
     quitPage: async (driver) => {
         await driver.quit();
     },
+    /**
+     * 
+     * @param {WebDriver} driver WebDriver client, which provides control over a browser
+     * @returns Array of window handles.
+     */
     getAllWindows: async(driver) => {
         const windows = await driver.getAllWindowHandles();
         return windows;
     },
+    /**
+     * 
+     * @param {WebDriver} driver WebDriver client, which provides control over a browser
+     * @param {Array} allWindows Array of window handles.
+     * @param {Number} positionPage position of windows
+     */
     switchPage: async(driver, allWindows, positionPage) => {
         await driver.switchTo().window(allWindows[positionPage]);
     },
+    /**
+     * 
+     * @param {WebDriver} driver WebDriver lient, which provides control over a browser.
+     * @param {Number} timeToWait time in seconds to wait the element
+     */
     implicitWait: async(driver, timeToWait) => {
         await driver.sleep(timeToWait);
     },
+    /**
+     * 
+     * @param {WebDriver} driver WebDriver client, which provides control over a browser
+     * @param {WebElementCondition} condition Defines a condition that will result in a WebElement
+     * @param {Number} timeToWait Time in seconds to wait the element
+     * @returns Web element
+     */
     explicitWaitElement: async(driver, condition, timeToWait) => {
         const element = await driver.wait(driver => condition.fn(driver), timeToWait);
         return element;
     },
+    /**
+     * 
+     * @param {WebDriver} driver WebDriver client, which provides control over a browser
+     * @param {WebElementCondition} condition Defines a condition that will result in a WebElement
+     * @param {Number} timeToWait Time in seconds to wait the element
+     * @param {String} message Message that appear when there is any error 
+     * @param {Number} timeToCheck Time in second to check the condition before timeToWait expires
+     * @returns Web element
+     */
     fluentWaitElement: async(driver, condition, timeToWait, message = "ERROR", timeToCheck = 500) => {
         const element = await driver.wait(driver => condition.fn(driver), timeToWait, message, timeToCheck);
         return element;
     },
+    /**
+     * 
+     * @param { WebDriver } driver WebDriver client, which provides control over a browser.
+     * @param { WebElement } element Web element
+     */
     scriptClick: async(driver, element) => {
         await driver.executeScript("arguments[0].click();", element);
     }
