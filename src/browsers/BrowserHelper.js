ffe49142ecdc6fd1ad6c52e1c3d467026757824c
jrichardsz <jrichardsz.java@gmail.com>
2024-04-12 20:31:44 -0500

    success firefox download, todo: windows test, driver download, auto configure browser and driver

--- src/browsers/BrowserHelper.js
+++ src/browsers/BrowserHelper.js
@@ -6,6 +6,8 @@ const finder = require("find-package-json");
 const https = require("https");
 const extract = require("extract-zip");
 const jp = require("jsonpath");
+const tar = require("tar-fs");
+const bzip = require("unbzip2-stream");
 
 function BrowserHelper() {
   var metadata = {
@@ -22,6 +24,12 @@ function BrowserHelper() {
             unpackedFolderName: "chrome-linux64",
           },
         },
+        win32: {
+          x64: {
+            url: "https://storage.googleapis.com/chrome-for-testing-public/#version/win64/chrome-win64.zip",
+            unpackedFolderName: "chrome-win64",
+          },
+        },
       },
     },
     firefox: {
@@ -35,20 +43,19 @@ function BrowserHelper() {
           x64: {
             url: "https://ftp.mozilla.org/pub/firefox/releases/#version/linux-x86_64/en-US/firefox-#version.tar.bz2",
             unpackedFolderName: "firefox",
-          }
+          },
+        },
+        win32: {
+          x64: {
+            url: "https://ftp.mozilla.org/pub/firefox/candidates/#version-candidates/build1/win32/en-US/firefox-#version.zip",
+            unpackedFolderName: "firefox",
+          },
         },
       },
     },
   };
 
-  //https://product-details.mozilla.org/1.0/firefox_versions.json
-  //LATEST_FIREFOX_VERSION
-  //https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/
-  //https://archive.mozilla.org/pub/firefox/releases/119.0.1/win32/en-US/
-  //https://archive.mozilla.org/pub/firefox/releases/124.0.2/linux-x86_64/en-US/
-  //https://ftp.mozilla.org/pub/firefox/candidates/124.0.2-candidates/build1/win32/en-US/firefox-124.0.2.zip
-
-  this.download = async (browser, buildId) => {
+  this.downloadBrowser = async (browser, buildId) => {
     var platform = os.platform();
     var arch = process.arch;
     try {
@@ -73,34 +80,51 @@ function BrowserHelper() {
     console.log(version);
     var downloadUrl =
       metadata[browser]["downloadUrlTemplate"][platform][arch]["url"];
-    downloadUrl = downloadUrl.replace("#version", version);
+    downloadUrl = downloadUrl.replace(/#version/g, version);
     console.log("browser to download: " + downloadUrl);
 
     var f = finder(__filename);
     var frameworkLocation = path.dirname(f.next().filename);
-    var destinationBrowserFolder = path.join(frameworkLocation, `.${browser}`, version, platform, arch);
+    var destinationBrowserFolder = path.join(
+      frameworkLocation,
+      `.${browser}`,
+      version,
+      platform,
+      arch
+    );
 
     if (!fs.existsSync(destinationBrowserFolder)) {
       await fs.promises.mkdir(destinationBrowserFolder, { recursive: true });
     }
 
-    var zipDestination = path.join(destinationBrowserFolder, downloadUrl.split("/").pop());
-    var unzipDestination = destinationBrowserFolder;
-    await downloadFileFomrUrl(downloadUrl, zipDestination);
-    await extract(zipDestination, { dir: unzipDestination });
+    var compressedFileLocation = path.join(
+      destinationBrowserFolder,
+      downloadUrl.split("/").pop()
+    );
+    var unpackDestination = destinationBrowserFolder;
+    await downloadFileFomrUrl(downloadUrl, compressedFileLocation);
+    
+    if(compressedFileLocation.endsWith(".zip")){
+        await extract(compressedFileLocation, { dir: unpackDestination });
+    }else if(compressedFileLocation.endsWith(".tar.bz2")){
+        await extractTar(compressedFileLocation, unpackDestination);
+    }else{
+        throw new Error("Unsupported file compression. Allowed: zip, .tar.bz2");
+    }
+
     var unpackedFolderName =
       metadata[browser]["downloadUrlTemplate"][platform][arch][
         "unpackedFolderName"
       ];
     var binaryLocation = path.join(
-      unzipDestination,
+      unpackDestination,
       unpackedFolderName,
-      "chrome" + (platform == "win32" ? ".exe" : "")
+      browser + (platform == "win32" ? ".exe" : "")
     );
     console.log("Extraction complete. Binary location: " + binaryLocation);
     //delete the downloaded files (zip, etc)
-    await fs.promises.rm(zipDestination, { recursive: true });  
-    return {binaryLocation};
+    await fs.promises.rm(compressedFileLocation, { recursive: true });
+    return { binaryLocation };
   }
 
   async function downloadFileFomrUrl(url, dest) {
@@ -115,6 +139,16 @@ function BrowserHelper() {
       });
     });
   }
+
+  async function extractTar(tarPath, folderPath) {
+    return new Promise((fulfill, reject) => {
+      const tarStream = tar.extract(folderPath);
+      tarStream.on('error', reject);
+      tarStream.on('finish', fulfill);
+      const readStream = fs.createReadStream(tarPath);
+      readStream.pipe(bzip()).pipe(tarStream);
+    });
+  }  
 }
 
 module.exports = BrowserHelper;
--- tests/Browser/BrowserHelper.test.js
+++ tests/Browser/BrowserHelper.test.js
@@ -3,12 +3,25 @@ const fs = require("fs")
 const BrowserHelper = require("../../src/browsers/BrowserHelper.js");
 
 describe('BrowserHelper', () => {
-    test.only('should download chrome binary', async () => {
-
-        await fs.promises.rm(path.join(process.cwd(), ".chrome"), { recursive: true });  
+    test('should download chrome binary', async () => {
+        var browserLocation = path.join(process.cwd(), ".chrome");
+        if (fs.existsSync(browserLocation)) {
+            await fs.promises.rm(browserLocation, { recursive: true });  
+        }            
         var browserHelper = new BrowserHelper();
-        var response = await browserHelper.download("chrome", "stable");
+        var response = await browserHelper.downloadBrowser("chrome", "stable");
         var exist = fs.existsSync(response.binaryLocation);
         expect(exist).toBe(true)
     });
+
+    test('should download firefox binary', async () => {
+        var browserLocation = path.join(process.cwd(), ".firefox");
+        if (fs.existsSync(browserLocation)) {
+            await fs.promises.rm(browserLocation, { recursive: true });  
+        }  
+        var browserHelper = new BrowserHelper();
+        var response = await browserHelper.downloadBrowser("firefox", "stable");
+        var exist = fs.existsSync(response.binaryLocation);
+        expect(exist).toBe(true)
+    });    
 });
